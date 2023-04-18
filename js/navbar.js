c588b7804a61120d44ab1cc31ee292960be2e6dd
KID-P <pedrocbva@gmail.com>
2023-04-18 03:14:49 +0100

    Enhance navigation style

--- js/navbar.js
+++ js/navbar.js
@@ -1,33 +1,52 @@
 const iris = document.getElementById("iris");
 
-document.onmousemove = () => {
-  let xAxis = (event.clientX * 100) / window.innerWidth;
-  let yAxis = (event.clientY * 100) / window.innerHeight;
+const moveEye = () => {
+  if (window.innerWidth > 768) {
+    document.onmousemove = () => {
+      let xAxis = (event.clientX * 100) / window.innerWidth;
+      let yAxis = (event.clientY * 100) / window.innerHeight;
 
-  if (yAxis < 40) {
-    yAxis = 40;
-  }
-  if (yAxis > 76) {
-    yAxis = 76;
-  }
+      if (yAxis < 40 && xAxis < 30) {
+        yAxis = 50;
+        xAxis = 50;
+      }
 
-  if (xAxis < 20) {
-    xAxis = 20;
-  }
-  if (xAxis > 84) {
-    xAxis = 84;
-  }
+      if (yAxis < 40) {
+        yAxis = 40;
+      }
+      if (yAxis > 76) {
+        yAxis = 76;
+      }
 
-  iris.style.left = `${xAxis}%`;
-  iris.style.top = `${yAxis}%`;
-  iris.style.transform = `translate(-${xAxis}%, -${yAxis})%`;
+      if (xAxis < 20) {
+        xAxis = 20;
+      }
+      if (xAxis > 84) {
+        xAxis = 84;
+      }
+
+      iris.style.left = `${xAxis}%`;
+      iris.style.top = `${yAxis}%`;
+      iris.style.transform = `translate(-${xAxis}%, -${yAxis})%`;
+    };
+  } else {
+    setInterval(() => {
+      let xAxis = Math.floor(Math.random() * (84 - 20 + 1)) + 20;
+      let yAxis = Math.floor(Math.random() * (76 - 40 + 1)) + 40;
+
+      iris.style.left = `${xAxis}%`;
+      iris.style.top = `${yAxis}%`;
+      iris.style.transform = `translate(-${xAxis}%, -${yAxis})%`;
+    }, 2500);
+  }
 };
 
-const navbar = document.getElementsByClassName("navbar")[0];
+const navbar = document.getElementsByClassName("navbar-wrapper")[0];
 const mobileNavbar = document.getElementsByClassName("navbar-mobile")[0];
 const mobileAction = document.getElementsByClassName("mobile")[0];
 const eyeNavbar = document.getElementById("eye");
 const bodyElement = document.getElementsByTagName("body")[0];
+const home = document.getElementById("home");
 
 let lastScrollY = window.scrollY;
 
@@ -42,12 +61,18 @@ window.addEventListener("scroll", () => {
 });
 
 const toggleMobileNavbar = () => {
-  mobileAction.classList.add("close");
+  if (mobileAction.classList.contains("open")) {
+    mobileAction.classList.remove("open");
+    mobileAction.classList.add("close");
+  }
   eyeNavbar.style.visibility = "hidden";
   bodyElement.style.overflow = "hidden";
 
   if (mobileNavbar.classList.contains("show")) {
-    mobileAction.classList.remove("close");
+    if (mobileAction.classList.contains("close")) {
+      mobileAction.classList.remove("close");
+      mobileAction.classList.add("open");
+    }
     eyeNavbar.style.visibility = "visible";
     bodyElement.style.overflow = "auto";
   }
@@ -57,9 +82,28 @@ const toggleMobileNavbar = () => {
 
 const closeMobileNavbar = () => {
   if (mobileNavbar.classList.contains("show")) {
-    mobileAction.classList.remove("close");
+    if (mobileAction.classList.contains("close")) {
+      mobileAction.classList.remove("close");
+      mobileAction.classList.add("open");
+    }
     mobileNavbar.classList.remove("show");
     eyeNavbar.style.visibility = "visible";
     bodyElement.style.overflow = "auto";
   }
 };
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+moveEye();
+observer.observe(home);
